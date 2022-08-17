b8abe7a2f7b8bdd6f4b9858f5ce15bb10c30da92
pavan <Pavanpk123@gmail.com>
2022-08-17 19:05:24 +0530

    assignment

---
+++ number inbuilt function.js
@@ -0,0 +1,20 @@
+var num = 10
+console.log(typeof(num))
+console.log(num.toString())
+console.log(typeof(num.toString()))
+
+var str ='10'
+console.log(typeof(str))
+console.log(parseInt(str))
+console.log(typeof(parseInt(str)))
+
+var strNum='10.5'
+console.log(strNum)
+console.log(typeof(strNum))
+console.log(parseFloat(strNum))
+
+var num=10.326548
+console.log(num)
+console.log(num.toFixed(3))
+console.log(typeof(num))
+console.log(num.toFixed(12))
---
+++ string.js
@@ -0,0 +1,18 @@
+// var myStr="java script"
+// console.log(myStr)
+// console.log(typeof(myStr))
+
+// var myStr="java script is a \'scripting\' language"
+// console.log(myStr)
+// var myStr2="java script is a \"scripting\"language"
+// console.log(myStr2)
+
+var str="java scripting is a scripting language"
+console.log(str.length)
+
+var str="java scripting is a scripting language"
+console.log(str.indexOf('script'))
+console.log(str.indexOf('language'))
+
+var str="java scripting is a scripting language string"
+console.log(str.lastIndexOf('string'))
